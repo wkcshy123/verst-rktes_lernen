5429ded4fb86c388b34b759045c493b6871e05ea
feng zhu <wkcshy80484906@hotmail.com>
2020-03-21 11:30:22 +0100

    no message

---
+++ lern1.py
@@ -0,0 +1,225 @@
+#倒立摆
+import gym
+env = gym.make('CartPole-v1')
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
+
+
+#小车
+env = gym.make('CarRacing-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample())
+env.close
+
+#环境列表
+from gym import envs
+print(envs.registry.all())
+
+
+#机器人
+import gym
+env = gym.make('BipedalWalker-v2')
+for i_episode in range(100):
+    observation = env.reset()
+    for t in range(10000):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("{} timesteps taken for the episode".format(t+1))
+            break
+
+
+import gym
+import universe  # register universe environment
+import random
+
+env = gym.make('flashgames.NeonRace-v0')
+env.configure(remotes=1) # automatically creates a local docker container
+observation_n = env.reset()
+
+## Declare actions
+# Move left
+left = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', True),
+        ('KeyEvent', 'ArrowRight', False)]
+right = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', False),
+         ('KeyEvent', 'ArrowRight', True)]
+
+# Move forward
+forward = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowRight', False),
+           ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'n', True)]
+
+turn = 0
+
+rewards = []
+
+buffer_size = 100
+
+action = forward
+
+while True:
+    turn -= 1
+    if turn <= 0:
+       action = forward
+       turn = 0
+    action_n = [action for ob in observation_n]
+    observation_n, reward_n, done_n, info = env.step(action_n)
+    rewards += [reward_n[0]]
+
+    if len(rewards) >= buffer_size:
+        mean = sum(rewards)/len(rewards)
+        if mean == 0:
+            turn = 20
+            if random.random() < 0.5:
+                action = right
+            else:
+                action = left
+        rewards = []
+    env.render()
+
+import gym
+import universe  # register the universe environments
+
+env = gym.make('flashgames.DuskDrive-v0')
+env.configure(remotes=1)  # automatically creates a local docker container
+observation_n = env.reset()
+
+while True:
+  action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]  # your agent here
+  observation_n, reward_n, done_n, info = env.step(action_n)
+  env.render()
+
+
+import tensorflow as tf
+hello = tf.constant("Hello World")
+sess = tf.Session()
+print(sess.run(hello))
+
+a = tf.constant(5)
+b = tf.constant(4)
+c = tf.multiply(a,b)
+d = tf.constant(2)
+e = tf.constant(3)
+f = tf.multiply(d,e)
+g = tf.add(c,f)
+
+with tf.Session() as sess:
+    writer = tf.summary.FileWriter("output", sess.graph)
+    print(sess.run(g))
+    writer.close()
+
+
+import gym
+import numpy as np
+
+env = gym.make("MountainCar-v0")
+
+LEARNING_RATE = 0.1
+
+DISCOUNT = 0.95
+EPISODES = 25000
+SHOW_EVERY = 3000
+
+DISCRETE_OS_SIZE = [20, 20]
+discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
+
+# Exploration settings
+epsilon = 1  # not a constant, qoing to be decayed
+START_EPSILON_DECAYING = 1
+END_EPSILON_DECAYING = EPISODES//2
+epsilon_decay_value = epsilon/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
+
+
+q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
+
+
+def get_discrete_state(state):
+    discrete_state = (state - env.observation_space.low)/discrete_os_win_size
+    return tuple(discrete_state.astype(np.int))  # we use this tuple to look up the 3 Q values for the available actions in the q-table
+
+
+for episode in range(EPISODES):
+    discrete_state = get_discrete_state(env.reset())
+    done = False
+
+    if episode % SHOW_EVERY == 0:
+        render = True
+        print(episode)
+    else:
+        render = False
+
+    while not done:
+
+        if np.random.random() > epsilon:
+            # Get action from Q table
+            action = np.argmax(q_table[discrete_state])
+        else:
+            # Get random action
+            action = np.random.randint(0, env.action_space.n)
+
+
+        new_state, reward, done, _ = env.step(action)
+
+        new_discrete_state = get_discrete_state(new_state)
+
+        if episode % SHOW_EVERY == 0:
+            env.render()
+        #new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+
+        # If simulation did not end yet after last step - update Q table
+        if not done:
+
+            # Maximum possible Q value in next step (for new state)
+            max_future_q = np.max(q_table[new_discrete_state])
+
+            # Current Q value (for current state and performed action)
+            current_q = q_table[discrete_state + (action,)]
+
+            # And here's our equation for a new Q value for current state and action
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+
+            # Update Q table with new Q value
+            q_table[discrete_state + (action,)] = new_q
+
+
+        # Simulation ended (for any reson) - if goal position is achived - update Q value with reward directly
+        elif new_state[0] >= env.env.goal_position:
+            #q_table[discrete_state + (action,)] = reward
+            q_table[discrete_state + (action,)] = 0
+
+        discrete_state = new_discrete_state
+
+    # Decaying is being done every episode if episode number is within decaying range
+    if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
+        epsilon -= epsilon_decay_value
+
+
+env.close()
+
+
+import gym
+import numpy as np
+
+
+env = gym.make('FrozenLake-v0')
+print(env.observation_space.n)
+print(env.action_space.n)
+
+value_table = np.zeros(env.observation_space.n)
+no_of_iterations = 100000
+
+for i in range(no_of_iterations):
+    updated_value_table = np.copy(value_table)
+
